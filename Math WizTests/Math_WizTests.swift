00be69881d1485be494ad84f8ba3393250ed8cb7
OwenLaRosa <owen.larosa@outlook.com>
2017-06-05 20:17:58 -0400

    Initial Commit

---
+++ Math WizTests/Math_WizTests.swift
@@ -0,0 +1,36 @@
+//
+//  Math_WizTests.swift
+//  Math WizTests
+//
+//  Created by Owen LaRosa on 6/5/17.
+//  Copyright © 2017 Owen LaRosa. All rights reserved.
+//
+
+import XCTest
+@testable import Math_Wiz
+
+class Math_WizTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
